224529fdc1aa52659a91153bbb4f2b09b9dc24fa
jatinagrawal36 <59972064+jatinagrawal36@users.noreply.github.com>
2020-10-02 14:43:27 +0530

    Update Length_of_longest_substring_without_repeating_char.cpp

--- Length_of_longest_substring_without_repeating_char.cpp
+++ Length_of_longest_substring_without_repeating_char.cpp
@@ -1,3 +1,8 @@
+/*
+Firstly I will check wether element is first occur or it is occured befor already
+Then if it is occured first time then i will stor it in map and change max2 accordingly 
+else if is occured earlier then i will remove it from map and increase my second pointer and after that i will that element again as my first pointer remians same in this case
+*/
 int lengthOfLongestSubstring(string s) {
         int i=0,j=0;
        unordered_map<int,int>mp;
