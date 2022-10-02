3091f699314674b79e101e951d9a64b1bbc7fcd6
Arijit Das <59313848+Arijit-Das-07@users.noreply.github.com>
2022-10-02 19:14:35 +0530

    Update Dijkstra.c
    Please Add Add the hacktoberfest-accepted lable

--- Algorithm/Dijkstra.c
+++ Algorithm/Dijkstra.c
@@ -3,21 +3,6 @@
 #define INFINITY 9999
 #define MAX 10
  
-void dijikstra(int G[MAX][MAX], int n, int startnode);
- 
-void main(){
-	int G[MAX][MAX], i, j, n, u;
-	printf("\nEnter the no. of vertices:: ");
-	scanf("%d", &n);
-	printf("\nEnter the adjacency matrix::\n");
-	for(i=0;i < n;i++)
-		for(j=0;j < n;j++)
-			scanf("%d", &G[i][j]);
-	printf("\nEnter the starting node:: ");
-	scanf("%d", &u);
-	dijikstra(G,n,u);
-}
- 
 void dijikstra(int G[MAX][MAX], int n, int startnode)
 {
 	int cost[MAX][MAX], distance[MAX], pred[MAX];
@@ -71,3 +56,18 @@ void dijikstra(int G[MAX][MAX], int n, int startnode)
 			while(j!=startnode);
 		}
 }
+ 
+void main(){
+	int G[MAX][MAX], i, j, n, u;
+	printf("\nEnter the no. of vertices:: ");
+	scanf("%d", &n);
+	printf("\nEnter the adjacency matrix::\n");
+	for(i=0;i < n;i++)
+		for(j=0;j < n;j++)
+			scanf("%d", &G[i][j]);
+	printf("\nEnter the starting node:: ");
+	scanf("%d", &u);
+	dijikstra(G,n,u);
+}
+ 
+
